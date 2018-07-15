8e0420b23c4245b6a95d5bd4d97e54be3542c14a
asantamaria <asantamaria@iri.upc.edu>
2018-07-15 19:32:37 +0200

    Improve build script

--- build.sh
+++ build.sh
@@ -1,9 +1,28 @@
+echo "Installing Pangolin dependencies: GLEW, Python 2.7"
+
+sudo apt-get install libglew-dev
+sudo apt-get install libpython2.7-dev
+
+echo "Installing Pangolin"
+
+cd Thirdparty
+git clone https://github.com/stevenlovegrove/Pangolin.git
+cd Pangolin
+mkdir build
+cd build
+cmake .. -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -Wall"
+cmake --build .
+sudo make install
+cd ..
+cd ..
+cd ..
+
 echo "Configuring and building Thirdparty/DBoW2 ..."
 
 cd Thirdparty/DBoW2
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_WARN_DEPRECATED=False
 make -j
 
 cd ../../g2o
@@ -12,7 +31,7 @@ echo "Configuring and building Thirdparty/g2o ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_WARN_DEPRECATED=False
 make -j
 
 cd ../../../
@@ -27,5 +46,14 @@ echo "Configuring and building ORB_SLAM2 ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_WARN_DEPRECATED=False
 make -j
+
+#echo "Installing and configuring ROS packages"
+#cd ..
+#PWD=eval pwd
+#echo "export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:$PWD/Examples/ROS" >> ~/.bashrc
+#source ~/.bashrc
+#eval chmod +x $PWD/build_ros.sh
+#source $PWD/build_ros.sh
+
